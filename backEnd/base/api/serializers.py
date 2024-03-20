60b54188242f9c4ac15302ec05aed4dd6b8e7eed
RezaAstaraki <reza.astaraky@gmail.com>
2024-03-21 00:10:39 +0330

    setup base api

---
+++ backEnd/base/api/serializers.py
@@ -0,0 +1,4 @@
+from rest_framework.serializers import ModelSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import JsonResponse
