52d73254ee42545ed4c3bb94cb6508b4fbc16ec1
Omidi-a <omidiazam325@gmail.com>
2024-03-22 14:40:29 +0330

    customized access token add user to it

--- backEnd/base/api/urls.py
+++ backEnd/base/api/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 from . import views
+from .serializers import MyTokenObtainPairSerializer
+
+
+
+
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 urlpatterns = [
     path('', views.get_routes, name='get_routes'),
 
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 
