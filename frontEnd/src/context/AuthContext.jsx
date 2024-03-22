ee87f40b621bdce35ffcebe839ab73033f40b2d7
Omidi-a <omidiazam325@gmail.com>
2024-03-22 13:36:14 +0330

    add login function

--- frontEnd/src/context/AuthContext.jsx
+++ frontEnd/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 const AuthContext = createContext();
 
@@ -6,7 +6,33 @@ export const AuthContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [authenticate, setAuthenticate] = useState(null);
 
-  const contextData = { user: user, authenticate: null };
+  const login = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: { "Context-Type": "application/json" },
+      body: {
+        username: e.target.username.value,
+        password: e.target.password.value,
+      },
+    });
+    if (response.status === 200) {
+      data = await response.json();
+      console.log("data", data);
+    } else {
+      alert("something wrong");
+    }
+  };
+
+  // useEffect(() => {
+  //   login();
+  // });
+
+  const contextData = {
+    user: user,
+    authenticate: null,
+    loginFunction: login,
+  };
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
   );
--- frontEnd/src/pages/login/Login.jsx
+++ frontEnd/src/pages/login/Login.jsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./login.scss";
+import AuthContext from "../../context/AuthContext";
 
 function Login() {
+  const { loginFunction } = useContext(AuthContext);
   return (
     <div className="login">
-      <form action="" method="post">
+      <form action="" method="post" onSubmit={(e) => loginFunction(e)}>
         <fieldset>
           <legend>Login</legend>
-          <input type="text" name="username" id="" placeholder="User Name" />
-          <input type="password" name="password" id="" placeholder="Password" />
+          <input
+            required={true}
+            type="text"
+            name="username"
+            id=""
+            placeholder="User Name"
+          />
+          <input
+            required={true}
+            type="password"
+            name="password"
+            id=""
+            placeholder="Password"
+          />
         </fieldset>
         <input type="submit" value="Login" />
       </form>
