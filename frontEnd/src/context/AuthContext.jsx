a08fb43a0d4c349271470364b039740ed8d08b7d
RezaAstaraki <reza.astaraky@gmail.com>
2024-03-22 19:08:14 +0330

    refresh tokens every four minutes

--- frontEnd/src/context/AuthContext.jsx
+++ frontEnd/src/context/AuthContext.jsx
@@ -4,11 +4,64 @@ import { jwtDecode } from "jwt-decode";
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
+  let firstRender = true;
   const storedToken = JSON.parse(localStorage.getItem("token"));
-  // const navigate = useNavigate();
 
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState(storedToken);
+
+  //define logout function
+  const logout = () => {
+    setToken(null);
+    setUser(null);
+    localStorage.removeItem("token");
+  };
+
+  //define refresh token
+  const refreshToken = async () => {
+    if (!token) {
+      return;
+    }
+    const response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: token.refresh,
+      }),
+    });
+    if (response.status === 200) {
+      const data = await response.json();
+      setToken(data);
+      setUser(jwtDecode(data.access).user);
+      localStorage.setItem("token", JSON.stringify(data));
+    } else {
+      setToken(null);
+      setUser(null);
+      localStorage.removeItem("token");
+    }
+  };
+
+  // refresh token every four minutes
+  useEffect(() => {
+    const fourMinutes = 1000 * 60 * 4;
+    const interval = setInterval(() => {
+      refreshToken();
+    }, fourMinutes);
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  // checking for first render
+  useEffect(() => {
+    console.log("firstRender", firstRender);
+    if (firstRender) {
+      refreshToken();
+    }
+    firstRender = false;
+  }, []);
 
   //define login function
   const login = async (e) => {
@@ -36,6 +89,7 @@ export const AuthContextProvider = ({ children }) => {
     user: user,
     authenticate: null,
     loginFunction: login,
+    logoutFunction: logout,
   };
   return (
     <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
