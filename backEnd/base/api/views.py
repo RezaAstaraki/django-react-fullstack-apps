fc564fdf8150e27009e30e4b34d9f15bd4476a45
RezaAstaraki <reza.astaraky@gmail.com>
2024-04-02 17:16:29 +0330

    add to api get routes

--- backEnd/base/api/views.py
+++ backEnd/base/api/views.py
@@ -1,10 +1,9 @@
-from rest_framework.decorators import api_view,permission_classes
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from django.http import JsonResponse
 from ..models import Notes
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-
 
 
 from .serializers import *
@@ -14,15 +13,15 @@ from .serializers import *
 def get_routes(request):
     routes = [
         '/api/token',
-        '/api/token/refresh'
+        '/api/token/refresh',
+        '<str:username>/notes/',
     ]
     return Response(routes)
 
 
-
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
-def notes_view(request,username):
+def notes_view(request, username):
     # r_user = request.user
     # print('<<<<<<<<<<<>>>>>>>>>>>>>>>>>')
     # print('user in request',r_user)
@@ -31,5 +30,5 @@ def notes_view(request,username):
     notes = User.objects.get(username=username).notes_set.all()
     # print(notes)
     # print('<<<<<<<<<<<>>>>>>>>>>>>>>>>>')
-    serialized_notes = NoteSerializer(notes,many=True)
+    serialized_notes = NoteSerializer(notes, many=True)
     return Response(serialized_notes.data)
